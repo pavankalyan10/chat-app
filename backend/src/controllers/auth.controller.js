e63c19f139e27bd1129b068f232c80655c43f019
pavan Kalyan <kalyanpavan64991@gmail.com>
2026-01-25 13:25:25 +0530

    bug fix

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -23,7 +23,7 @@ export const signup = async (req, res) => {
     if (user) return res.status(400).json({ message: "Email already exists" });
 
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password.salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new User({
       fullName,
